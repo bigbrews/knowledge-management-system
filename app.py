33a998d5270dfb9cf0e75b5a2cf18de2b8f87fd9
chrstn-sgrr <131838456+chrstn-sgrr@users.noreply.github.com>
2023-05-12 15:01:48 +0800

    feat: added logged in selection page

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def log_in():
 def register():
     return render_template('logged_out/register.html')
 
+@app.route('/selection')
+def selection():
+    return render_template('logged_in/selection.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
